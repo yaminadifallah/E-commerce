cceb51301011b3c354a28dd8718848b36ba6f29b
Yamina Difallah <yaminadifallah03@gmail.com>
2026-09-20 22:34:39 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,24 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     },
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['successful-fascination-production-ccad.up.railway.app'],
   },
 });
